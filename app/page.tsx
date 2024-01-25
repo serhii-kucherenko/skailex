0515ebb33fd90b121251871616e78cb0e1a0afa2
Serhii Kucherenko <kucherenko.web@gmail.com>
2024-01-25 00:04:15 -0800

    Update text

--- app/page.tsx
+++ app/page.tsx
@@ -29,21 +29,21 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Skailex
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
+          We Drive Digital Transformation through Web and Mobile Expertise
+        </h2>
+        <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://www.linkedin.com/in/serhii-kucherenko/"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
-        </h2>
+            Join Now
+            ></Link>
       </div>
     </div>
   );
